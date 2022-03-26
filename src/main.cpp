326542a4419bacd74d5319c61642338fd1d2a4ff
674xu8 <enginseer@enginseer.ru>
2022-03-26 14:16:05 +0700

    add wifi

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #define BUFFER_SIZE 100
-//GV
-const char *ssid = "xxxxxx"; // Имя роутера
-const char *pass = "xxxxxx"; // Пароль роутера
+// GV
+const char *ssid = "xxxxxx";       // Имя роутера
+const char *pass = "xxxxxx";       // Пароль роутера
 const char *mqtt_server = "xxxxx"; // Имя сервера MQTT
-const int mqtt_port = 00000;                   // Порт для подключения к серверу MQTT
-const char *mqtt_user = "xxxxxx";              // Логи для подключения к серверу MQTT
-const char *mqtt_pass = "xxxxxx";              // Пароль для подключения к серверу MQTT
+const int mqtt_port = 00000;       // Порт для подключения к серверу MQTT
+const char *mqtt_user = "xxxxxx";  // Логи для подключения к серверу MQTT
+const char *mqtt_pass = "xxxxxx";  // Пароль для подключения к серверу MQTT
 const char *topic = "test";
 const int pause = 1000; //пауза между отправками
 int tm = 300;
@@ -16,12 +16,13 @@ int tm = 300;
 WiFiClient wifiC;
 
 PubSubClient client(mqtt_server, 1883, wifiC);
-//PFP
+// PFP
 void refreshData();
 
 void setup()
 {
   Serial.begin(115200);
+  WiFi.begin(ssid, pass);
   if (client.connect("esp32", mqtt_user, mqtt_pass))
   {
     client.publish(topic, "hello world");
@@ -43,6 +44,5 @@ void refreshData()
     tm = pause; // пауза меду отправками 3 секунды
   }
   tm--;
-
   delay(1);
 }
